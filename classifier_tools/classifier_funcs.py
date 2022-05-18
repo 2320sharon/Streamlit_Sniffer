a0d9d9ae05f37325bf2eecb2b576a5a6076c4ca1
Sharon Fitzpatrick <SF2309@Gmail.Com>
2022-05-17 17:44:42 -0700

    New sniffer with classifier code working predict

---
+++ classifier_tools/classifier_funcs.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+from numpy import asarray
+import pandas as pd
+
+
+def create_predictions_df(predictions,images):
+    labeled_predictions=list(map(lambda x: "good "if x>=0.5 else "bad",predictions))
+    # If no predictions have been done before add all the image predictions to the dataframe
+    if st.session_state.pred_df.empty:
+        for i,img in enumerate(images):
+            row={"Filename":img.name,'Sorted':labeled_predictions[i],'Probability':predictions[i]}
+            st.session_state.pred_df=pd.concat([st.session_state.pred_df,pd.DataFrame.from_records([row])],ignore_index=True)
+    # If predictions have been done before only add new entries and modify old entries if the prediction changed
+    else: 
+        for i,img in enumerate(images):
+                existing_entry=st.session_state.pred_df.loc[st.session_state.pred_df["Filename"] == img.name,"Sorted"]
+                if existing_entry.empty == False: #There is an existing prediction for this image
+                    old_label=existing_entry.to_string(index=False)
+                    if old_label != labeled_predictions[i]: 
+                        row=[img.name,labeled_predictions[i],predictions[i]]
+                        st.session_state.pred_df.loc[st.session_state.pred_df["Filename"] == img.name]=row
+                else: #No entry exists for this image name
+                    row={"Filename":img.name,'Sorted':labeled_predictions[i],'Probability':predictions[i]}
+                    st.session_state.pred_df=pd.concat([st.session_state.pred_df,pd.DataFrame.from_records([row])],ignore_index=True)
+
+def create_prediction_ready_data(images_list:list,img_shape:tuple):
+    """returns a numpy array of the shape (number_images,img_shape,3)"""
+    images=[]
+    for image in images_list:
+        img=Image.open(image)
+        img_array=pre_process_img(img,img_shape)
+        images.append(img_array)
+    data=np.vstack(images)
+    return data
+
+
+def pre_process_img(image,img_shape:tuple)->list:
+    """returns np.array resized to (1,img_shape,3)"""
+    img=image.resize(img_shape,Image.ANTIALIAS)
+    imgArray = asarray(img)
+    imgArray=imgArray.reshape((1,)+img_shape+(3,))# Create batch axis
+    return imgArray
+
+
+def show_curr_image_classifier(images_list):
+    # Display a the done image when the all images have been displayed
+    if st.session_state.img_idx>=len(images_list):
+        image = Image.open("./assets/done.jpg")
+        st.image(image,width=300)
+    elif images_list==[]:
+        image= Image.open("./assets/new_loading_sniffer.jpg")
+    else:
+        st.write("Index",st.session_state.img_idx)
+        if st.session_state.pred_df.empty ==False:
+            curr_pred=st.session_state.pred_df[st.session_state.pred_df["Filename"]==images_list[st.session_state.img_idx].name]
+            st.write("Predicted as ",curr_pred["Sorted"].iloc[0],"with ",curr_pred["Probability"].iloc[0])
+        # caption is "" when images_list is empty otherwise its image name 
+        caption = '' if images_list==[] else f'#{st.session_state.img_idx} {images_list[st.session_state.img_idx].name}'
+        image = Image.open(images_list[st.session_state.img_idx])
+        st.image(image, caption=caption,width=300)
+
+
+def view_prediction_images(images_list):
+    if st.session_state.pred_df.empty ==False:
+        images_per_row=st.slider("The number of images per row",step=1,value=4,min_value=1,max_value=8)
+        n_rows=len(images_list)/images_per_row
+        n_rows=int(np.ceil(n_rows)) #round up because range is end exclusive
+        for row_num in range(n_rows):
+            cols=st.columns(images_per_row)
+            start=row_num*images_per_row
+            end=start+images_per_row
+            if end>len(images_list):
+                end = len(images_list)
+            for col,image in zip(cols,images_list[start:end]):
+                curr_pred=st.session_state.pred_df[st.session_state.pred_df["Filename"]==image.name]
+                predicted_label=curr_pred["Sorted"].iloc[0]
+                col.image(image,use_column_width=True,caption=f"{predicted_label}")
--- sniffer.py
+++ sniffer.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import os
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from PIL import Image
 from datetime import datetime
+from classifier_tools import classifier_funcs
 
 st.set_page_config(
      page_title="Sniffer",
@@ -15,6 +18,16 @@ st.set_page_config(
          'About': "# Sniffer. Sort your *extremely* cool images!"
      }
  )
+
+# Load the tensorflow model only once
+@st.experimental_singleton
+def get_model(model_name:str,model_path:str=os.getcwd()+ os.sep+"models"):
+    model_path+=os.sep+model_name
+    model = tf.keras.models.load_model(model_path+os.sep+"model")
+    return model
+
+# Load the model from the singleton cache
+model=get_model("binary_classification_model_v_2_1")
 
 with st.expander("See Upload", expanded=True):
     uploaded_files = st.file_uploader("Choose a jpg file", accept_multiple_files=True)
@@ -36,22 +49,41 @@ def create_csv_name(csv_filename:str=None)->str:
 # Initialize Sniffer's states
 if 'img_idx' not in st.session_state:
     st.session_state.img_idx=0
-if 'df' not in st.session_state:
-    st.session_state.df=pd.DataFrame(columns=['Filename','Sorted','Index'])
+if 'choice_df' not in st.session_state:
+    st.session_state.choice_df=pd.DataFrame(columns=['Filename','Sorted'])
+if 'pred_df' not in st.session_state:
+    st.session_state.pred_df=pd.DataFrame(columns=['Filename','Sorted','Probability'])
 
 # img_idx will always be inside images_list
-if st.session_state.img_idx > (len(images_list)):
+if st.session_state.img_idx > (len(images_list)+2):
     st.session_state.img_idx = (len(images_list)-1) if (len(images_list)-1)>0 else 0
 
-def create_csv():
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    st.session_state.df.to_csv().encode('utf-8')
-    return st.session_state.df.to_csv().encode('utf-8')
+def create_csv(df,df2=None):
+    if  not isinstance(df2,type(None)):
+        if df2.empty == False and df.empty == False:
+            return pd.concat([df,df2],ignore_index=True).to_csv(index=False).encode('utf-8')
+        elif df2.empty == False and df.empty == True:
+            return df2.to_csv(index=False).encode('utf-8')
+    return df.to_csv(index=False).encode('utf-8')
+
+
+def predict():
+    # Make sure the images list is not empty and the index is valid
+    if images_list != [] and st.session_state.img_idx <= len(images_list)-1:
+        start =0 if st.session_state.img_idx <= 0 else st.session_state.img_idx
+        end=len(images_list)-1
+        img_shape=(100,100)
+        if start <= end:
+            data=classifier_funcs.create_prediction_ready_data(images_list[start:end+1],img_shape)
+            predictions=model.predict(data)
+            predictions=predictions.flatten().tolist()
+            classifier_funcs.create_predictions_df(predictions,images_list[start:end+1])
+
 
 def yes_button():
     if -1 < st.session_state.img_idx <= (len(images_list)-1)   :
-        row={"Filename":images_list[st.session_state.img_idx].name,'Sorted':"good",'Index':st.session_state.img_idx}
-        st.session_state.df=pd.concat([st.session_state.df,pd.DataFrame.from_records([row])],ignore_index=True)
+        row={"Filename":images_list[st.session_state.img_idx].name,'Sorted':"good"}
+        st.session_state.choice_df=pd.concat([st.session_state.choice_df,pd.DataFrame.from_records([row])],ignore_index=True)
         st.session_state.img_idx += 1
     elif st.session_state.img_idx ==(len(images_list)):
         st.success('All images have been sorted!')
@@ -62,8 +94,8 @@ def yes_button():
 
 def no_button():
     if -1 < st.session_state.img_idx <= (len(images_list)-1) :
-        row={"Filename":images_list[st.session_state.img_idx].name,'Sorted':"bad",'Index':st.session_state.img_idx}
-        st.session_state.df=pd.concat([st.session_state.df,pd.DataFrame.from_records([row])],ignore_index=True)
+        row={"Filename":images_list[st.session_state.img_idx].name,'Sorted':"bad"}
+        st.session_state.choice_df=pd.concat([st.session_state.choice_df,pd.DataFrame.from_records([row])],ignore_index=True)
         st.session_state.img_idx += 1
     elif st.session_state.img_idx == (len(images_list)):
         st.success('All images have been sorted!')
@@ -77,8 +109,8 @@ def undo_button():
         st.session_state.img_idx -= 1
         if images_list != [] and st.session_state.img_idx <= (len(images_list)-1) :
             drop_filename=images_list[st.session_state.img_idx].name
-            index=st.session_state.df.loc[st.session_state.df['Filename'] == drop_filename].index.values
-            st.session_state.df.drop(index, axis=0, inplace=True)
+            index=st.session_state.choice_df.loc[st.session_state.choice_df['Filename'] == drop_filename].index.values
+            st.session_state.choice_df.drop(index, axis=0, inplace=True)
     else:
         st.warning('Cannot Undo')
 
@@ -108,7 +140,7 @@ with col1:
     st.button(label="Yes",key="yes_button",on_click=yes_button)
     st.button(label="No",key="no_button",on_click=no_button)
     st.button(label="Undo",key="undo_button",on_click=undo_button)
-    
+    st.button(label="✨ Predict ✨",key="predict_button",on_click=predict)
 with col2:
     # Display done.jpg when all images are sorted 
     if st.session_state.img_idx>=len(images_list):
@@ -121,12 +153,24 @@ with col2:
     
 with col4:
     st.download_button(
-     label="Download data as CSV 💻",
-     data=create_csv(),
+     label="Download choices as CSV 🦮", 
+     data=create_csv( st.session_state.choice_df),
      file_name= create_csv_name(),
-     mime='text/csv',
- )
+     mime='text/csv',)
+
+    st.download_button(
+     label="Download predictions as CSV ✨",
+     data=create_csv(st.session_state.pred_df),
+     file_name= create_csv_name(),
+     mime='text/csv',)
+
+    st.download_button(
+     label="Download predictions & choices as CSV 💻",
+     data=create_csv(st.session_state.choice_df,st.session_state.pred_df),
+     file_name= create_csv_name(),
+     mime='text/csv',)
 
 with st.expander("See Dataset Details 📈"):
-    st.dataframe(st.session_state.df)
-    st.bar_chart(st.session_state.df['Sorted'].value_counts())
+    st.dataframe(st.session_state.pred_df)
+    st.dataframe(st.session_state.choice_df)
+    st.bar_chart(st.session_state.choice_df['Sorted'].value_counts())
