9176bda935a2996a7f8eda524291b9cf38fd84bf
2320sharon <sharonfitzpatrick@csus.edu>
2022-05-31 11:42:22 -0700

    working version tested on 2000 images

--- sniffer.py
+++ sniffer.py
@@ -6,66 +6,35 @@ from PIL import Image
 from datetime import datetime
 
 st.set_page_config(
-     page_title="Sniffer",
-     page_icon="🐕",
-     layout="centered",
-     initial_sidebar_state="collapsed",
-     menu_items={
-         'Get Help': "https://github.com/2320sharon/Streamlit_Sniffer",
-         'Report a bug': "https://github.com/2320sharon/Streamlit_Sniffer/issues",
-         'About': "# Sniffer. Sort your *extremely* cool images!"
-     }
- )
+    page_title="Sniffer",
+    page_icon="🐕",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': "https://github.com/2320sharon/Streamlit_Sniffer",
+        'Report a bug': "https://github.com/2320sharon/Streamlit_Sniffer/issues",
+        'About': "# Sniffer. Sort your *extremely* cool images!"
+    }
+)
 
-def get_percent_blk_pixels(img):
-    img_array=np.array(img)
-    w,h=img_array.shape[0:2]
-    total_pixels=w*h if img_array.ndim<3 else w*h*img_array.shape[2]
+
+def get_percent_blk_pixels(file):
+    img = Image.open(file)
+    img_array = np.array(img)
+    w, h = img_array.shape[0:2]
+    total_pixels = w * h if img_array.ndim < 3 else w * h * img_array.shape[2]
     black_count = np.sum(img_array == 0)
-    return np.round(black_count/total_pixels,3)
+    return np.round(black_count / total_pixels, 3)
 
 
-if 'length_images_list' not in st.session_state:
-    st.session_state.length_images_list=-1
-if 'blk_pxl_dict' not in st.session_state:
-    st.session_state.blk_pxl_dict={}
-if 'remove_imgs' not in st.session_state:
-    st.session_state.remove_imgs=False
-
-def remove_images_button():
-    st.session_state.remove_imgs = not st.session_state.remove_imgs
-
-with st.expander("Upload Images",expanded=True):
+with st.expander("Upload Images", expanded=True):
     uploaded_files = st.file_uploader("Choose a jpg file", accept_multiple_files=True)
     for uploaded_file in uploaded_files:
         bytes_data = uploaded_file.read()
-    max_blk_percent = st.slider("Percentage of Black Pixels Allowed:", key="upload",step=0.01, value=0.50, min_value=0.0, max_value=1.0)
-    st.write("Would you like to remove the images that exceed the maximum percentage of black pixels?")
-    st.button(label="Remove Images",key="remove_images",on_click=remove_images_button)
-    if st.session_state.remove_imgs == True:
-        idx_remove=[]
-        for cnt,file in enumerate(uploaded_files):
-            img = Image.open(file)
-            blk_pixels=get_percent_blk_pixels(img)
-            if blk_pixels > max_blk_percent:
-                idx_remove.append(cnt)
-        for idx in reversed(idx_remove):
-             del uploaded_files[idx]
-    
-    images_list=uploaded_files
-                
+    images_list = uploaded_files
 
-if st.session_state.length_images_list != len(images_list):
-    st.session_state.length_images_list=len(images_list)
-    for file in uploaded_files:
-        if file.name not in st.session_state.blk_pxl_dict.keys():
-            img = Image.open(file)
-            blk_pixels=get_percent_blk_pixels(img)
-            st.session_state.blk_pxl_dict[file.name]=blk_pixels
-            
-st.write(st.session_state.blk_pxl_dict)
 
-def create_csv_name(csv_filename:str=None)->str:
+def create_csv_name(csv_filename: str = None) -> str:
     today = datetime.now()
     if csv_filename is not None:
         if not csv_filename.endswith(".csv"):
@@ -75,15 +44,18 @@ def create_csv_name(csv_filename:str=None)->str:
         csv_filename = f"Sniffer_Output_" + d1 + ".csv"
     return csv_filename
 
+
 # Initialize Sniffer's states
 if 'img_idx' not in st.session_state:
-    st.session_state.img_idx=0
+    st.session_state.img_idx = 0
+if 'undo_indexes' not in st.session_state:
+    st.session_state.undo_indexes = [0]
 if 'df' not in st.session_state:
-    st.session_state.df=pd.DataFrame(columns=['Filename','Sorted','Index'])
+    st.session_state.df = pd.DataFrame(columns=['Filename', 'Sorted'])
 
 
-if st.session_state.img_idx > (len(images_list)+2):
-    st.session_state.img_idx = (len(images_list)-1) if (len(images_list)-1)>0 else 0
+if st.session_state.img_idx > (len(images_list) + 2):
+    st.session_state.img_idx = (len(images_list) - 1) if (len(images_list) - 1) > 0 else 0
 
 
 def create_csv():
@@ -92,39 +64,72 @@ def create_csv():
     return st.session_state.df.to_csv().encode('utf-8')
 
 
-def yes_button():
-    if -1 < st.session_state.img_idx <= (len(images_list)-1)   :
-        if st.session_state.blk_pxl_dict[images_list[st.session_state.img_idx].name] <= blk_percent:
-            row={"Filename":images_list[st.session_state.img_idx].name,'Sorted':"good",'Index':st.session_state.img_idx}
-            st.session_state.df=pd.concat([st.session_state.df,pd.DataFrame.from_records([row])],ignore_index=True)
+def increment_index(view_images_box):
+    """increments the st.session_state.img_idx to the next valid image index depending on the checkbox
+    Args:
+        view_images_box (bool): Checkbox to view images with black pixel% > limit is active
+    """
+    if view_images_box:
         st.session_state.img_idx += 1
-    elif st.session_state.img_idx ==(len(images_list)):
+    elif not view_images_box:
+        # Add the current index as a valid index to list of valid index to undo to
+        st.session_state.undo_indexes.append(st.session_state.img_idx)
+        if get_percent_blk_pixels(images_list[st.session_state.img_idx]) > blk_percent:
+            # Continue incrementing index while in range and percnt_blk_pixels > limit
+            while (-1 < st.session_state.img_idx <= (len(images_list) - 1)
+                   ) and (get_percent_blk_pixels(images_list[st.session_state.img_idx]) > blk_percent):
+                st.session_state.img_idx += 1
+        else:
+            st.session_state.img_idx += 1
+
+
+def index_out_of_range(idx, length):
+    if idx == (length):
         st.success('All images have been sorted!')
         st.balloons()
     else:
-        st.warning(f'No more images to sort { st.session_state.img_idx} /{ len(images_list)} ')
+        st.warning(f'No more images to sort {idx} /{length} ')
 
 
-def no_button():
-    if -1 < st.session_state.img_idx <= (len(images_list)-1) :
-        if st.session_state.blk_pxl_dict[images_list[st.session_state.img_idx].name] <= blk_percent:
-            row={"Filename":images_list[st.session_state.img_idx].name,'Sorted':"bad",'Index':st.session_state.img_idx}
-            st.session_state.df=pd.concat([st.session_state.df,pd.DataFrame.from_records([row])],ignore_index=True)
-        st.session_state.img_idx += 1
-    elif st.session_state.img_idx == (len(images_list)):
-        st.success('All images have been sorted!')
-        st.balloons()
+def yes_button(**kwargs):
+    view_images_box = kwargs["checkbox"]
+    if -1 < st.session_state.img_idx <= (len(images_list) - 1):
+        if get_percent_blk_pixels(images_list[st.session_state.img_idx]) <= blk_percent:
+            row = {"Filename": images_list[st.session_state.img_idx].name, 'Sorted': "good"}
+            st.session_state.df = pd.concat([st.session_state.df, pd.DataFrame.from_records([row])], ignore_index=True)
+        increment_index(view_images_box)
     else:
-        st.warning(f'No more images to sort { st.session_state.img_idx}/{ len(images_list)}')
+        # Handles all cases when index is out of range
+        index_out_of_range(st.session_state.img_idx, len(images_list))
 
 
-def undo_button():
-    if st.session_state.img_idx >0:
+def no_button(**kwargs):
+    view_images_box = kwargs["checkbox"]
+    if -1 < st.session_state.img_idx <= (len(images_list) - 1):
+        if get_percent_blk_pixels(images_list[st.session_state.img_idx]) <= blk_percent:
+            row = {"Filename": images_list[st.session_state.img_idx].name, 'Sorted': "bad"}
+            st.session_state.df = pd.concat([st.session_state.df, pd.DataFrame.from_records([row])], ignore_index=True)
+        increment_index(view_images_box)
+    else:
+        # Handles all cases when index is out of range
+        index_out_of_range(st.session_state.img_idx, len(images_list))
+
+
+def undo_button(**kwargs):
+    view_images_box = kwargs["checkbox"]
+    if view_images_box:
         st.session_state.img_idx -= 1
-        if images_list != [] and st.session_state.img_idx <= (len(images_list)-1) :
-            drop_filename=images_list[st.session_state.img_idx].name
-            index=st.session_state.df.loc[st.session_state.df['Filename'] == drop_filename].index.values
-            st.session_state.df.drop(index, axis=0, inplace=True)
+    elif not view_images_box:
+        st.session_state.img_idx = st.session_state.undo_indexes.pop()
+        # Ensure the list of available undo indexes are never empty
+        if len(st.session_state.undo_indexes) == 0:
+            st.session_state.undo_indexes.append(0)
+    # Remove filename from the dataframe
+    if st.session_state.img_idx > 0:
+        if images_list != [] and st.session_state.img_idx <= (len(images_list) - 1):
+            drop_filename = images_list[st.session_state.img_idx].name
+            drop_index = st.session_state.df.loc[st.session_state.df['Filename'] == drop_filename].index.values
+            st.session_state.df.drop(drop_index, axis=0, inplace=True)
     else:
         st.warning('Cannot Undo')
 
@@ -133,51 +138,53 @@ st.title("Sniffer🐕")
 st.image("./assets/sniffer.jpg")
 
 # Sets num_image=1 if images_list is empty
-num_images=(len(images_list)) if (len(images_list))>0 else 1
+num_images = (len(images_list)) if (len(images_list)) > 0 else 1
 
 try:
-    my_bar = st.progress((st.session_state.img_idx)/num_images)
+    my_bar = st.progress((st.session_state.img_idx) / num_images)
 except st.StreamlitAPIException:
     my_bar = st.progress(0)
 
 
-col1,col2,col3,col4=st.columns(4)
+col1, col2, col3, col4 = st.columns(4)
 with col1:
     blk_percent = st.slider("Percentage of Black Pixels Allowed:", step=0.01, value=0.50, min_value=0.0, max_value=1.0)
-    st.write(blk_percent)
-    st.button(label="Yes",key="yes_button",on_click=yes_button)
-    st.button(label="No",key="no_button",on_click=no_button)
-    st.button(label="Undo",key="undo_button",on_click=undo_button)
-    
+    view_images_box = st.checkbox("See Images that exceed percentage of black pixel limit (They won't be sorted)")
+    st.button(label="Yes", key="yes_button", on_click=yes_button, kwargs={"checkbox": view_images_box})
+    st.button(label="No", key="no_button", on_click=no_button, kwargs={"checkbox": view_images_box})
+    st.button(label="Undo", key="undo_button", on_click=undo_button, kwargs={"checkbox": view_images_box})
+
 with col2:
     st.write(f"{st.session_state.img_idx} of {num_images}")
-    if images_list==[]:
-        image= Image.open("./assets/new_loading_sniffer.jpg")
+    if images_list == []:
+        image = Image.open("./assets/new_loading_sniffer.jpg")
     else:
-        # Display done.jpg when all images are sorted 
-        if st.session_state.img_idx>=len(images_list):
+        # Display done.jpg when all images are sorted
+        if st.session_state.img_idx >= len(images_list):
             image = Image.open("./assets/done.jpg")
-            st.image(image,width=300)
+            st.image(image, width=300)
         else:
-            st.session_state.blk_pxl_dict[images_list[st.session_state.img_idx].name]
-            if st.session_state.blk_pxl_dict[images_list[st.session_state.img_idx].name] > blk_percent:
-                st.write("This image exceeds the allowed \% black pixels. It will not be sorted.")
-            if st.session_state.img_idx>=len(images_list):
+            percent_blk_pixels = get_percent_blk_pixels(images_list[st.session_state.img_idx])
+            st.write(f"Percentage of Black Pixels : {round(percent_blk_pixels*100,4)}%")
+            if percent_blk_pixels > blk_percent:
+                st.write("This image exceeds limit of allowed black_pixels. It will not be sorted.")
+            if st.session_state.img_idx >= len(images_list):
                 image = Image.open("./assets/done.jpg")
-                st.image(image,width=300)
+                st.image(image, width=300)
             else:
-                # caption is "" when images_list is empty otherwise its image name 
+                # caption is "" when images_list is empty otherwise its image name
                 image = Image.open(images_list[st.session_state.img_idx])
-                caption = '' if images_list==[] else f'#{st.session_state.img_idx} {images_list[st.session_state.img_idx].name}'
-                st.image(image, caption=caption,width=300)
-    
+                caption = '' if images_list == [
+                ] else f'#{st.session_state.img_idx} {images_list[st.session_state.img_idx].name}'
+                st.image(image, caption=caption, width=300)
+
 with col4:
     st.download_button(
-     label="Download data as CSV 💻",
-     data=create_csv(),
-     file_name= create_csv_name(),
-     mime='text/csv',
- )
+        label="Download data as CSV 💻",
+        data=create_csv(),
+        file_name=create_csv_name(),
+        mime='text/csv',
+    )
 
 with st.expander("See Dataset Details 📈"):
     st.dataframe(st.session_state.df)
